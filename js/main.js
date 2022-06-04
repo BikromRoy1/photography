4d2ab2a2569ac41ac347dfba0250670ee72dff5a
Bikrom Roy <bikrom@piit.local>
2022-06-04 11:36:11 +0600

    update featuer section

---
+++ js/main.js
@@ -0,0 +1,59 @@
+(function ($) {
+	
+	"use strict";
+
+	// WOW JS
+	$(window).on ('load', function (){
+        if ($(".wow").length) { 
+            var wow = new WOW ({
+                boxClass:     'wow',      // Animated element css class (default is wow)
+                animateClass: 'animated', // Animation css class (default is animated)
+                offset:       20,         // Distance to the element when triggering the animation (default is 0)
+                mobile:       true,       // Trigger animations on mobile devices (default is true)
+                live:         true,       // Act on asynchronously loaded content (default is true)
+            });
+            wow.init();
+        }
+    });
+
+	$('.owl-testimonials').owlCarousel({
+		items:1,
+		loop:true,
+		dots: true,
+		nav: false,
+		autoplay: true,
+		margin:30,
+		  responsive:{
+			  0:{
+				  items:1
+			  },
+			  600:{
+				  items:1
+			  },
+			  1000:{
+				  items:1
+			  }
+		  }
+	  })
+
+	$('.owl-features').owlCarousel({
+		items:3,
+		loop:true,
+		dots: true,
+		nav: true,
+		autoplay: true,
+		margin:30,
+		  responsive:{
+			  0:{
+				  items:1
+			  },
+			  800:{
+				  items:2
+			  },
+			  1000:{
+				  items:3
+			  }
+		  }
+	  })
+	
+})(window.jQuery);
